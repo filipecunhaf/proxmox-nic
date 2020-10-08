aef37daf9c3bf8ef0e4e5e2be5676318855954b3
filipecunhaf <filipecunhaf@gmail.com>
2020-10-08 07:53:49 +0200

    Add files via upload

--- odroid.sh
+++ odroid.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-apt-get update
-apt-get upgrade
-
-apt install wget
-
-echo deb http://download.proxmox.com/debian/pve stretch pve-no-subscription > /etc/apt/sources.list.d/pve-enterprise.list
-
 chmod 1777 /tmp
+
+apt-get update
+apt-get -y upgrade
+
+apt install -y wget
+
+echo deb http://download.proxmox.com/debian/pve buster pve-no-subscription > /etc/apt/sources.list.d/pve-enterprise.list
 
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
 
-apt install pve-headers-$(uname -r) build-essential
+apt install -y pve-headers-$(uname -r) build-essential dkms
 
 wget https://github.com/awesometic/realtek-r8125-dkms/releases/download/9.003.05-1/realtek-r8125-dkms_9.003.05-1_amd64.deb -O r8125-dkms.deb
 
 dpkg -i r8125-dkms.deb
+
+ip addr show
